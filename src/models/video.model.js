51b185b37de939f4f36d67b065ab7b9b3a1e1b4c
utkrishta24 <krishsinha24062gmail.com>
2024-01-24 18:38:24 +0530

    multer setup

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -41,6 +41,6 @@ const videoSchema = new Schema(
   },
 );
 
-videoSchema.plugin(mongooseAggregatePaginate)
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.models("Video", videoSchema);
